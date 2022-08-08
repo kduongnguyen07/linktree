e3130c935b3e1902aa78cdc4bca59d2ec5f49f68
kduongnguyen07 <100816706+kduongnguyen07@users.noreply.github.com>
2022-08-08 20:45:06 +0700

    oke

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+$(window).load(function() {
+    $('#preload').delay(1000).fadeOut('fast', function() {
+        $('body').removeClass('preloading');
+    });
+});
 VanillaTilt.init(document.querySelectorAll(".social__links ul li"), {
     max: 10,
     speed: 800,
         glare: true,
         "max-glare": 1
   });
+/*Preloader*/
